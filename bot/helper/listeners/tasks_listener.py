44ad0d9bf8b896ac240032283c819cc450bd31ef
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2024-01-25 15:05:17 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -588,8 +588,7 @@ class MirrorLeechListener:
                 await start_from_queued()
                 return
         
-        if self.botpmmsg and (not config_dict['DELETE_LINKS'] or config_dict['CLEAN_LOG_MSG']):
-            await deleteMessage(self.botpmmsg)
+        
         
         await clean_download(self.dir)
         async with download_dict_lock:
