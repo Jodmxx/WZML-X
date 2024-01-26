b7f599679d7085899c5894e2f4cec118f90972d7
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2024-01-26 09:46:21 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -132,7 +132,7 @@ class WZMLStyle:
 ------------------------------------------\n\n"""
     
     # async def __msg_to_reply(self): ---> pyrogramEngine.py
-    PM_START =            "➲ <b><u>Task Started now :</u></b>\n┃\n┖ <b>Link:</b> {msg_link}"
+    PM_START =            "➲ <b><u>Task Started:</u></b>\n┃\n┖ <b>Link:</b> {msg_link}"
     L_LOG_START =           """➲ <b><u>Leech Started :</u></b>\n┃\n┠ <b>User :</b> {mention} ( #ID{uid} )\n┖ <b>Source :
     ------------------------------------------
 {msg_link}
